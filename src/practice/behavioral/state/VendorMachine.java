add15ae7af2c9f8a509369aecc9df0aa61b34e95
Subhatav Dhali <subhatav@gmail.com>
2024-11-02 20:56:26 +0530

    Implement the State Design Pattern

---
+++ src/practice/behavioral/state/VendorMachine.java
@@ -0,0 +1,30 @@
+package practice.behavioral.state;
+
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.java.Log;
+
+@Log
+@Setter
+@ToString
+@EqualsAndHashCode
+public final class VendorMachine {
+
+  private VendorState state = new NoCoinState();
+
+  public void insertMoney() {
+    log.info("ATTEMPT to INSERT Money => [" + this + "]");
+    state.insertCoin(this); // Try to INSERT Money
+  }
+
+  public void ejectMoney() {
+    log.info("ATTEMPT to EJECT Money => [" + this + "]");
+    state.ejectCoin(this); // Try to EJECT Money
+  }
+
+  public void dispenseFood() {
+    log.info("ATTEMPT to DISPENSE Food => [" + this + "]");
+    state.dispenseFood(this); // Try to DISPENSE Food
+  }
+}
